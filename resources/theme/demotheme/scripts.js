30c97b256600d89709c00d2b13fb1d466be3b685
Asko Soukka <asko.soukka@iki.fi>
2017-10-08 23:00:00 +0300

    Add imagesLoaded bundle to fix Masonry init only when images have been loaded

--- resources/theme/demotheme/scripts.js
+++ resources/theme/demotheme/scripts.js
@@ -1,8 +1,10 @@
 /* globals jQuery */
 jQuery(function($) {
   'use strict';
-  $('.wall-of-fame').masonry({
-    itemSelector: 'img',
-    percentPosition: true
-  });
+    $('.wall-of-fame').imagesLoaded(function() {
+        $('.wall-of-fame').masonry({
+            itemSelector: 'img',
+            percentPosition: true
+        });
+    });
 });
